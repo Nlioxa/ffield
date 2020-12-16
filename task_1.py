269002b6f18399c9dc0067028724d4fd9e0e3aa9
Nlioxa Korzun <36415737+Nlioxa@users.noreply.github.com>
2020-12-16 13:47:38 +0200

    Add task results in comments

--- task_1.py
+++ task_1.py
@@ -11,3 +11,10 @@ print("task 3: b = {0}\tc = {1}".format(f.find(b), f.find(c)))
 print("task 4: {0}".format(b * c))
 print("task 5: {0}".format(b / c))
 print("task 6: {0}".format(b ** c))
+
+# task 1: 21
+# task 2: 18
+# task 3: b = 3 c = 2
+# task 4: 46
+# task 5: 13
+# task 6: 5
